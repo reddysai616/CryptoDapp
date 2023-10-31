2189238420fb57be7056ee99577492453fbeb19e
saikiran0701 <reddysai616@gmail.com>
2023-10-31 15:01:30 +0530

    uncomments

--- Web3/scripts/deploy.js
+++ Web3/scripts/deploy.js
@@ -1,9 +1,4 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
+
 const hre = require("hardhat");
 
 const  main = async () => {
@@ -14,13 +9,6 @@ const  main = async () => {
   console.log(transactions.address , 'deployed')
 
 }
-// console.log(transactions.address , 'deployed')
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-// main().catch((error) => {
-//   console.error(error);
-//   process.exitCode = 1;
-// });
 
 const runMain = async () =>{
   try{
